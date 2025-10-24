0a5cd0eeefd669e786c829eaf8917208e219f366
pedrin <pedrinho.top07@gmail.com>
2025-10-23 22:54:16 -0300

    Initial commit for new learning Langchain4J + AI assistents

---
+++ src/main/java/com/pedrinspec/langchain4j/Langchain4JApplication.java
@@ -0,0 +1,13 @@
+package com.pedrinspec.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4JApplication {
+
+    void main(String[] args) {
+        SpringApplication.run(Langchain4JApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrinspec/langchain4j/Langchain4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrinspec.langchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
